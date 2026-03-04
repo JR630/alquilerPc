1bd50a9b4446d7a4715ed32da08654357fc6a185
Juan Raigosa <raikez630@gmail.com>
2026-03-04 07:32:16 -0500

    Actualización del sistema

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
+var idCliente = 1; 
 
 function calcular(){
     
     // VARIABLES
-    var idCliente = 1; 
     var valorDia = 35000;
     var equipos = parseInt(document.getElementById("equipos").value);
     var dias = parseInt(document.getElementById("dias").value);
     var diasAdicionales = parseInt(document.getElementById("diasAdicionales").value);
     var opcion = document.getElementById("opcion").value;
+    var usuario = document.getElementById("usuario").value;
+    var email = document.getElementById("email").value;
 
     var subtotalInicial;
     var subtotalAdicional;
     var descuentoAdicional;
+    var valorDescuentoAdicional = 0;
+    var valorDescuentoOIncremento = 0;
     var total;
     var mensaje;
-    var idCliente;
 
     if(equipos < 2){
         alert("Debe alquilar mínimo 2 equipos");
@@ -22,7 +25,7 @@ function calcular(){
     }
 
     idCliente = idCliente;
-    idCliente = idCliente + 1;
+    idCliente = idCliente +1;
 
     subtotalInicial = equipos * dias * valorDia;
     subtotalAdicional = equipos * diasAdicionales * valorDia;
@@ -33,27 +36,71 @@ function calcular(){
         descuentoAdicional = 0.20;
     }
 
-    subtotalAdicional = subtotalAdicional - (subtotalAdicional * descuentoAdicional);
+    valorDescuentoAdicional = subtotalAdicional * descuentoAdicional;
+    subtotalAdicional = subtotalAdicional - valorDescuentoAdicional;
 
     total = subtotalInicial + subtotalAdicional;
 
     if(opcion == "2"){
-        total = total + (total * 0.05);
+    valorDescuentoOIncremento = total * 0.05;
+    total = total + valorDescuentoOIncremento;
         mensaje = "Incremento del 5% por servicio fuera de la ciudad.";
     }
     else if(opcion == "3"){
-        total = total - (total * 0.05);
+    valorDescuentoOIncremento = total * 0.05;
+    total = total - valorDescuentoOIncremento;
         mensaje = "Descuento del 5% por alquiler en el establecimiento.";
     }
     else{
         mensaje = "Sin descuentos ni incrementos.";
     }
 
-    document.getElementById("resultado").innerHTML =
-        "ID Cliente: " + idCliente + "<br>" +
-        "Equipos: " + equipos + "<br>" +
-        "Días iniciales: " + dias + "<br>" +
-        "Días adicionales: " + diasAdicionales + "<br>" +
-        mensaje + "<br><br>" +
-        "<b>Total a pagar: $" + total.toLocaleString() + "</b>";
+    if(usuario == ""){
+    alert("Debe ingresar el nombre del cliente");
+    return;
+    }
+
+    for(var i = 0; i < usuario.length; i++){
+
+        var letra = usuario.charAt(i);
+
+        if(!(letra >= "a" && letra <= "z") &&
+        !(letra >= "A" && letra <= "Z") &&
+        letra != " "){
+
+            alert("El nombre solo debe contener letras");
+            return;
+        }
+    }
+
+
+    if(dias < 0){
+        alert("Los días iniciales no pueden ser negativos");
+        return;
+    }
+
+    if(diasAdicionales < 0){
+        alert("Los días adicionales no pueden ser negativos");
+        return;
+    }
+
+    if(email == ""){
+    alert("Debe ingresar un correo electrónico");
+    return;
 }
+
+    if(email.indexOf("@") == -1 || email.indexOf(".") == -1){
+        alert("El correo electrónico no tiene un formato válido");
+        return;
+    }
+    
+    document.getElementById("resultado").innerHTML =
+    "ID Cliente: " + idCliente + "<br>" +
+    "Nombre: " + usuario + "<br>" +
+    "Equipos: " + equipos + "<br>" +
+    "Días iniciales: " + dias + "<br>" +
+    "Días adicionales: " + diasAdicionales + "<br>" +
+    mensaje + "<br><br>" +
+    "<b>Total a pagar: $" + total.toLocaleString() + "</b>";
+}
+
